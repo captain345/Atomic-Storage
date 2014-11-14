839cb64ed86055fca9b070c1febc16437bc5e4c1
captain345 <thomas@thomass-mbp.uni.mdx.ac.uk>
2014-11-14 14:51:55 +0000

    Initial Push Front End

---
+++ AtomicStorage/file-index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+    <html>
+        <head lang="en">
+           <meta charset="UTF-8">
+            <title>Atomic Files</title>
+
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+            <link href = "css/bootstrap.min.css" rel="stylesheet">
+            <link href = "css/styles.css" rel="stylesheet">
+
+            <script src="js/jquery-2.1.1.min.js"></script>
+            <script src="js/bootstrap.js"></script>
+        </head>
+
+        <body>
+
+
+        </body>
+    </html>
